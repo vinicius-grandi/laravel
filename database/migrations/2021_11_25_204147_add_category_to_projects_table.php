6acbe2eeb0106fbf896a6088a025d71927479831
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2021-11-26 14:00:56 -0300

    estudadinha básica

--- database/migrations/2021_11_25_204147_add_category_to_projects_table.php
+++ database/migrations/2021_11_25_204147_add_category_to_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectsTable extends Migration
+class AddCategoryToProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
-            $table->string('nome');
-            $table->id();
-            $table->timestamps();
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('category', 100);
         });
     }
 
@@ -27,6 +25,8 @@ class CreateProjectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('category');
+        });
     }
 }
